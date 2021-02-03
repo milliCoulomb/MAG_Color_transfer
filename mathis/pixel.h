e9dacceb78e4a4d74fb7afa4dd3e40429d164df0
Universal-AC <root@localhost.localdomain>
2021-02-03 16:27:43 +0100

    Le début de la classe pixel

---
+++ mathis/pixel.h
@@ -0,0 +1,18 @@
+#ifndef _PIXEL_H_
+#define _PIXEL_H_
+#include<iostream>
+#include<ostream>
+#include"Classematrice.h"
+using namespace std:
+class pixel : public matrice
+{
+ private :
+  int rouge;
+  int vert;
+  int bleu;
+ public :
+  pixel(int 3, int 1)
+ 
+ 
+    
+#endif //_PIXEL_H_
