050d3d121018b45a20560150f26760055f64df54
Julien-Gueret <julien.gueret22@outlook.fr>
2021-02-03 15:30:43 +0100

    Modification de matrice.h

--- Julien/Classematrice.h
+++ Julien/Classematrice.h
@@ -1,5 +1,37 @@
+#ifndef Matrice_
+#define Matrice_
 #include<iostream>
+#include<ostream>
 
 using namespace std :
 
-class matrice 
+class matrice {
+
+private :
+
+int NBligne;
+int NBcolonne;
+double **matrice;
+
+public :
+
+//Constructeur 
+matrice(const NBligne, const NBcolonne);
+
+//Destructeur 
+~matrice();
+
+// get et set 
+double get_NBligne() const;
+double get_NBcolonne() const;
+
+void set_NBligne(const int NBligne_);
+void set_NBcolonne(const int NBcolonne_);
+
+//Surcharge opérateur
+matrice & operator=(const matrice & matrice_);
+ostream & operator<<(ostream & out, const matrice & matrice_);
+
+//Produit matriciel 
+double produit(matrice matrice_);
+}
