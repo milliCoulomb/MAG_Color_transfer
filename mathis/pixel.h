0f9c22dd6028f81643a1bcd477bf5533fd1d9789
Universal-AC <root@localhost.localdomain>
2021-02-03 17:25:18 +0100

    Pixel suite

--- mathis/pixel.h
+++ mathis/pixel.h
@@ -7,13 +7,20 @@ using namespace std:
 class pixel : public matrice
 {
  private :
-  int rouge;
-  int vert;
-  int bleu;
+  int p_red;
+  int p_green;
+  int p_blue;
   int NBlines=3;
   int NBcol=0;
  public :
   pixel();
+  ~pixel();
+  int get_red() const;
+  int get_green() const;
+  int get_blue() const;
+  void set_red(int red);
+  void set_green(int green);
+  void set_blue(int blue);
   
  
  
