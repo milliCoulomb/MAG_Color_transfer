0159e77142ea660d5ed637062616208328de7613
Universal-AC <mathis.caprais@gmail.com>
2021-02-10 11:16:16 +0100

    Avec déclaration de matrix

--- pixel/pixel.cpp
+++ pixel/pixel.cpp
@@ -29,7 +29,7 @@ void pixel::set_blue(const double blue_) {
 }
 
 pixel::pixel(const int lines_, const int rows_) :
-	lines(lines_), rows(rows_)
+	lines(lines_), rows(rows_), matrix(lines, rows)
 {
 	new (this) matrix(lines, rows);
 	cout << this <<endl;
