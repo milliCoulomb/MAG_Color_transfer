7d43c917958ba069214a38ca31cba63adf532f25
Universal-AC <mathis.caprais@gmail.com>
2021-02-10 12:12:30 +0100

    En espèrant que ça marche

--- pixel/pixel.cpp
+++ pixel/pixel.cpp
@@ -56,6 +56,8 @@ pixel pixel::LMS(const pixel & pixel_) {
     for (int i=0; i<get_lines(); i++) {
 		for (int j=0; j<get_rows(); j++){
 			Y.tab[i][j]=Z.tab[i][j];
+		}
+	}
 	return Y;
 }
 pixel pixel::LAB(const pixel & pixel_) {
@@ -79,11 +81,17 @@ pixel pixel::LAB(const pixel & pixel_) {
 			// Passage en logarithme des couleurs.
 		}
 	}
-	pixel M2 = lab2.prod(pixel_);
-    pixel M3=lab1.prod(M2);
-	return M3;
+	matrix M2 = lab2.prod(pixel_);
+    matrix M3=lab1.prod(M2);
+    pixel Y;
+    for (int i=0; i<get_lines(); i++) {
+		for (int j=0; j<get_rows(); j++){
+			Y.tab[i][j]=M3.tab[i][j];
+		}
+	}
+	return Y;
 }
-pixel pixel::back_to_LMS_from_LAB() const {
+pixel pixel::back_to_LMS_from_LAB(const pixel & pixel_) const {
 	matrix lab1_inv(3,3);
 	lab1_inv.tab[0][0]=1/pow(3, 0.5);
 	lab1_inv.tab[1][1]=1/pow(6, 0.5);
@@ -98,10 +106,17 @@ pixel pixel::back_to_LMS_from_LAB() const {
 	lab2_inv.tab[2][0]=1.0;
 	lab2_inv.tab[2][1]=-2.0;
 	lab2_inv.tab[2][2]=0.0;
-	M3=lab1_inv.prod(pixel);
-	return lab2_inv.prod(M3);
+	M3=lab1_inv.prod(pixel_);
+	matrix M4 = lab2_inv.prod(M3);
+	pixel Y;
+    for (int i=0; i<get_lines(); i++) {
+		for (int j=0; j<get_rows(); j++){
+			Y.tab[i][j]=M4.tab[i][j];
+		}
+	}
+	return Y;
 }
-pixel pixel::back_to_RGB_from_LMS() const {
+pixel pixel::back_to_RGB_from_LMS(const pixel & pixel_) const {
 	matrix lms_inv(3,3);
 	lms_inv.tab[0][0]=4.4679;
 	lms_inv.tab[0][1]=-3.5873;
@@ -114,9 +129,16 @@ pixel pixel::back_to_RGB_from_LMS() const {
 	lms_inv.tab[2][2]=1.2045;
 	for (int i=0; i<get_lines(); i++) {
 		for (int j=0; j<get_rows(); j++){
-			pixel.tab[i][j]=exp(pixel.tab[i][j]);
+			pixel_.tab[i][j]=exp(pixel_.tab[i][j]);
 			//passage en exopnentiel des couleurs, retour vers l'espace RGB.
 		}
 	}
-	return lms_inv.prod(pixel);
+	matrix M=lms_inv.prod(pixel_)
+	pixel Y;
+    for (int i=0; i<get_lines(); i++) {
+		for (int j=0; j<get_rows(); j++){
+			Y.tab[i][j]=M.tab[i][j];
+		}
+	}
+	return Y;
 }
