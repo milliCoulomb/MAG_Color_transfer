341d1f80d65315a9c79841fee4bf559622a963dc
Universal-AC <mathis.caprais@gmail.com>
2021-03-07 10:29:52 +0100

    v2

--- color_transfer_v2/matrix.h
+++ color_transfer_v2/matrix.h
@@ -1,21 +1,21 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
 class matrix {
-
 private :
-
-int NBligne;
-int NBcolonne;
-
+	int NBligne;
+	int NBcolonne;
 public :
 
-long double **tab;
-matrix(const int NBligne_, const int NBcolonne_);
-virtual ~matrix();
-double get_NBligne() const;
-double get_NBcolonne() const;
-void set_NBligne(const int NBligne_);
-void set_NBcolonne(const int NBcolonne_);
-void affiche() const;
-matrix & operator=(const matrix & matrix_);
-matrix prod(const matrix & matrix_);
-matrix T() const;
+	long double **tab;
+	matrix(const int NBligne_, const int NBcolonne_);
+	virtual ~matrix();
+	double get_NBligne() const;
+	double get_NBcolonne() const;
+	void set_NBligne(const int NBligne_);
+	void set_NBcolonne(const int NBcolonne_);
+	void affiche() const;
+	matrix & operator=(const matrix & matrix_);
+	matrix prod(const matrix & matrix_);
+	matrix T() const;
 };
+#endif //_MATRIX_H_
